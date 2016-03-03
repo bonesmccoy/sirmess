ce499afdce0879228bdeded5455993565d1e30ee
Daniele Murroni <daniele.murroni@gmail.com>
2016-03-03 10:58:18 +0000

    messages first spec

---
+++ spec/Bones/SirMess/Model/MessageSpec.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace spec\Bones\SirMess\Model;
+
+use Bones\SirMess\Model\User;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class MessageSpec extends ObjectBehavior
+{
+    function let(User $user)
+    {
+        $user->beADoubleOf('Bones\SirMess\Model\User');
+        $this->beConstructedWith($user, 'title', 'body');
+
+    }
+
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Bones\SirMess\Model\Message');
+    }
+
+    function it_should_add_recipient()
+    {
+        $user = new User(3);
+        $this->addRecipient($user);
+        $this->getRecipients()->shouldHaveCount(1);
+    }
+
+    function it_should_add_another_recipient()
+    {
+        $user = new User(4);
+        $this->addRecipient($user);
+
+        $user = new User(5);
+        $this->addRecipient($user);
+
+        $this->getRecipients()->shouldHaveCount(2);
+    }
+
+    function it_shouldnt_add_a_recipient_twice()
+    {
+        $user = new User(3);
+        $this->addRecipient($user);
+        $this->addRecipient($user);
+        $this->getRecipients()->shouldHaveCount(1);
+    }
+
+    function it_can_be_marked_as_read()
+    {
+        $user = new User(3);
+        $this->addRecipient($user);
+        $this->markAsReadFromUser($user);
+    }
+
+    function it_can_be_marked_as_read_only_from_existing_recipients()
+    {
+        $user = new User(3);
+        $this->addRecipient($user);
+        $this->markAsReadFromUser($user);
+        $this->getReaders()->shouldHaveCount(1);
+
+        $user = new User(4);
+        $this->markAsReadFromUser($user);
+        $this->getReaders()->shouldHaveCount(1);
+    }
+
+
+
+
+}
---
+++ src/Bones/SirMess/Model/Message.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace Bones\SirMess\Model;
+
+
+class Message
+{
+
+    protected $_id;
+
+    protected $title;
+
+    protected $body;
+
+    protected $sender;
+
+
+    public function __construct(User $sender, $title, $body)
+    {
+
+        $this->sender = $sender;
+        $this->title = $title;
+        $this->body = $body;
+    }
+
+    /**
+     * @var User[]
+     */
+    protected $recipients = array();
+
+    /**
+     * @var User[]
+     */
+    protected $readers = array();
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSender()
+    {
+        return $this->sender;
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getRecipients()
+    {
+        return $this->recipients;
+    }
+
+
+    public function addRecipient(User $user)
+    {
+        $this->recipients[$user->getId()] = $user;
+    }
+
+
+    /**
+     * @return User[]
+     */
+    public function getReaders()
+    {
+        return $this->readers;
+    }
+
+
+    public function markAsReadFromUser(User $user)
+    {
+        if (isset($this->recipients[$user->getId()]) &&
+            !isset($this->readers[$user->getId()])
+        ) {
+            $this->readers[$user->getId()] = $user;
+        }
+    }
+
+
+}
