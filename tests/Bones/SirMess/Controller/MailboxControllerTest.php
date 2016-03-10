92cbff355a1522d876cb86940bc551b5257aca78
Daniele Murroni <daniele.murroni@gmail.com>
2016-03-10 16:01:12 +0000

    test outbox

--- tests/Bones/SirMess/Controller/MailboxControllerTest.php
+++ tests/Bones/SirMess/Controller/MailboxControllerTest.php
@@ -29,14 +29,51 @@ class MailboxControllerTest extends BaseWebTestCase
         $client = $this->createClient();
         $client->request('GET', '/mailbox/outbox/1');
         $response = $client->getResponse();
+        $this->assertTrue($client->getResponse()->isOk());
 
         $this->assertEquals(
             'application/json',
             $response->headers->get('content-type')
         );
 
+        $conversationList = json_decode($response->getContent(), true);
 
-        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertCount(
+            1,
+            $conversationList
+        );
+
+        $conversation = current($conversationList);
+
+        $this->assertArrayHasKey(
+            'messageList',
+            $conversation
+        );
+
+        $foundMessageAsSender = false;
+        foreach($conversation['messageList'] as $message) {
+            if (isset($message['sender']['id']) && $message['sender']['id'] == 1) {
+                $foundMessageAsSender = true;
+                break;
+            }
+        }
+
+        $this->assertTrue($foundMessageAsSender, "No message found in the conversation with person 1 as sender");
+
+        $this->assertArrayHasKey(
+            'personList',
+            $conversation
+        );
+
+        $foundPersonInConversation = false;
+        foreach($conversation['personList'] as $person) {
+            if ($person['id'] == 1) {
+                $foundPersonInConversation = true;
+                break;
+            }
+        }
+
+        $this->assertTrue($foundPersonInConversation, "No person 1 found in conversation person list");
 
     }
 
